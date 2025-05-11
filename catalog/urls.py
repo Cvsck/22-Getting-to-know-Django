685cbcc79021bb745c3a8289541867b3cb89d21d
Максим Пестов <moikrym@mail.ru>
2025-05-11 12:43:19 +0300

    Everything is working1

--- catalog/urls.py
+++ catalog/urls.py
@@ -10,18 +10,4 @@ urlpatterns = [
         TemplateView.as_view(template_name="contacts.html"),
         name="contacts",
     ),
-    path(
-        "catalog/", TemplateView.as_view(template_name="catalog.html"), name="catalog"
-    ),
-    path("orders/", TemplateView.as_view(template_name="orders.html"), name="orders"),
-    path(
-        "profile/", TemplateView.as_view(template_name="profile.html"), name="profile"
-    ),
-    path("logout/", TemplateView.as_view(template_name="logout.html"), name="logout"),
-    path(
-        "settings/",
-        TemplateView.as_view(template_name="settings.html"),
-        name="settings",
-    ),
-    path("help/", TemplateView.as_view(template_name="help.html"), name="help"),
 ]
